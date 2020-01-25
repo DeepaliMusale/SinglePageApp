b0db5181bf00bfd42bc09498c5a06fccb6990257
Deepali Musale <deepalimusale3@gmail.com>
2020-01-25 11:21:52 -0600

    Updates

---
+++ precache-manifest.659847ef975aa92af1b5c607dd1473c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d949b5264ab83bb1445ca90486b9aa7",
+    "url": "/SinglePageApp/index.html"
+  },
+  {
+    "revision": "4999cf4fe045bc234ddf",
+    "url": "/SinglePageApp/static/css/main.bbe7e6d8.chunk.css"
+  },
+  {
+    "revision": "65d36f645195ec249a82",
+    "url": "/SinglePageApp/static/js/2.52be6784.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SinglePageApp/static/js/2.52be6784.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4999cf4fe045bc234ddf",
+    "url": "/SinglePageApp/static/js/main.4e6b10a0.chunk.js"
+  },
+  {
+    "revision": "04bf049a8d35cea840f0",
+    "url": "/SinglePageApp/static/js/runtime-main.593d2850.js"
+  }
+]);
